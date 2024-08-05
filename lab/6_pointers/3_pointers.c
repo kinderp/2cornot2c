d3a5bf97cf86bd5a9ef3c001c94647a0f598a32c
acari <antonio.caristia@davincimilazzo.edu.it>
2024-08-05 11:28:46 +0200

    Add 3_pointers.c

--- lab/6_pointers/3_pointers.c
+++ lab/6_pointers/3_pointers.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(void){
-	int *p = NULL;
-	if (p != NULL)
+	int *p = NULL; /* inizializzo il puntatore p a NULL */
+	if (p != NULL)	/* prima di deferenziare controllo se p è diverso da NULL */
 		printf("*p = %d", *p);
 
 }
